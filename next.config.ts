479b7d07007afb265056948306433ed10f4fdceb
Hang Dev <hang.dev1210@gmail.com>
2025-12-18 15:11:10 +0800

    Setup build config for github

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enables static exports
+  images: {
+    unoptimized: true, // Prevents image optimization that requires a server
+  },
 };
 
 export default nextConfig;
